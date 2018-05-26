c1b80880ee662e4e31984245d893213f9e99c116
Steve Brownlee <chortlehoort@gmail.com>
2018-05-25 19:13:23 -0500

    KP

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('honestabe.json');
+const router = jsonServer.router('kittypurry.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
